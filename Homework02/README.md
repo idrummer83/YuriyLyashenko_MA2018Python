c124f04139ef817ac07212c9c33e20e6bb738579
idrummer83 <vodaadminwater@gmail.com>
2018-11-21 00:36:56 +0200

    homework02 fix2

---
+++ Homework02/README.md
@@ -0,0 +1,2 @@
+
+https://py3.codeskulptor.org/#user302_c9zS3892RV_129.py
--- Homework02/index.py
+++ Homework02/index.py
@@ -17,14 +17,17 @@ guess_number = 0
 num_guesses = 7
 past = 0
 
+
 def new_secret():
     return random.randrange(0, 100)
+
 
 secret_number = new_secret()
 
 
 def input_guess(num):
     return int(num)
+
 
 frame = simplegui.create_frame('Guess Number', 100, 200)
 inp = frame.add_input('user_num', input_guess, 80)
@@ -35,11 +38,11 @@ def restart_new():
     global past
     past = 10
 
-    
+
 def restart(*args):
     print('game RESTARTED')
     print('')
-    inp.set_text('')    
+    inp.set_text('')
     global secret_number
     global num_guesses
     if len(args) > 0:
@@ -49,12 +52,12 @@ def restart(*args):
         num_guesses = 7
         secret_number = random.randrange(0, 100)
 
-        
+
 def remaining(*args):
     secret = secret_number
     guess = int(inp.get_text())
     print('Guess was ' + str(guess))
-        
+
     global num_guesses
     if num_guesses >= 0:
         print('Number of remaining guesses is ' + str(num_guesses))
@@ -63,10 +66,12 @@ def remaining(*args):
         print('You ran out of guesses. The number was ' + str(secret_number))
         if past:
             restart_new()
+            return False
         else:
             restart()
-            
-    mid = (secret + guess)//2
+            return False
+
+    mid = (secret + guess) // 2
     if mid > secret and mid < guess:
         print('Lower!')
         print('#')
@@ -79,142 +84,141 @@ def remaining(*args):
             restart_new()
         else:
             restart()
-        
+
+
 frame.add_button("Rangeis[0,100)", restart, 120)
 frame.add_button("Rangeis[0,1000)", restart_new, 120)
-frame.add_button("Guess", remaining, 100) 
+frame.add_button("Guess", remaining, 100)
 input_guess("50")
-
 
 ###################################################
 # Start our test #1 - assume global variable secret_number
 # is the the "secret number" - change name if necessary
 
-#input_guess("50")
-#input_guess("75")
-#input_guess("62")
-#input_guess("68")
-#input_guess("71")
-#input_guess("73")
-#input_guess("74")
+# input_guess("50")
+# input_guess("75")
+# input_guess("62")
+# input_guess("68")
+# input_guess("71")
+# input_guess("73")
+# input_guess("74")
 
 ###################################################
 # Output from test #1
-#New game. Range is [0,100)
-#Number of remaining guesses is 7
+# New game. Range is [0,100)
+# Number of remaining guesses is 7
 #
-#Guess was 50
-#Number of remaining guesses is 6
-#Higher!
+# Guess was 50
+# Number of remaining guesses is 6
+# Higher!
 #
-#Guess was 75
-#Number of remaining guesses is 5
-#Lower!
+# Guess was 75
+# Number of remaining guesses is 5
+# Lower!
 #
-#Guess was 62
-#Number of remaining guesses is 4
-#Higher!
+# Guess was 62
+# Number of remaining guesses is 4
+# Higher!
 #
-#Guess was 68
-#Number of remaining guesses is 3
-#Higher!
+# Guess was 68
+# Number of remaining guesses is 3
+# Higher!
 #
-#Guess was 71
-#Number of remaining guesses is 2
-#Higher!
+# Guess was 71
+# Number of remaining guesses is 2
+# Higher!
 #
-#Guess was 73
-#Number of remaining guesses is 1
-#Higher!
+# Guess was 73
+# Number of remaining guesses is 1
+# Higher!
 #
-#Guess was 74
-#Number of remaining guesses is 0
-#Correct!
+# Guess was 74
+# Number of remaining guesses is 0
+# Correct!
 #
-#New game. Range is [0,100)
-#Number of remaining guesses is 7
+# New game. Range is [0,100)
+# Number of remaining guesses is 7
 
 ###################################################
 # Start our test #2 - assume global variable secret_number
 # is the the "secret number" - change name if necessary
 
-#range1000()
-#secret_number = 375    
-#input_guess("500")
-#input_guess("250")
-#input_guess("375")
+# range1000()
+# secret_number = 375
+# input_guess("500")
+# input_guess("250")
+# input_guess("375")
 
 ###################################################
 # Output from test #2
-#New game. Range is [0,100)
-#Number of remaining guesses is 7
+# New game. Range is [0,100)
+# Number of remaining guesses is 7
 #
-#New game. Range is [0,1000)
-#Number of remaining guesses is 10
+# New game. Range is [0,1000)
+# Number of remaining guesses is 10
 #
-#Guess was 500
-#Number of remaining guesses is 9
-#Lower!
+# Guess was 500
+# Number of remaining guesses is 9
+# Lower!
 #
-#Guess was 250
-#Number of remaining guesses is 8
-#Higher!
+# Guess was 250
+# Number of remaining guesses is 8
+# Higher!
 #
-#Guess was 375
-#Number of remaining guesses is 7
-#Correct!
+# Guess was 375
+# Number of remaining guesses is 7
+# Correct!
 #
-#New game. Range is [0,1000)
-#Number of remaining guesses is 10
-
+# New game. Range is [0,1000)
+# Number of remaining guesses is 10
 
 
 ###################################################
 # Start our test #3 - assume global variable secret_number
 # is the the "secret number" - change name if necessary
 
-#range100()
-#secret_number = 28 
-#input_guess("50")
-#input_guess("50")
-#input_guess("50")
-#input_guess("50")
-#input_guess("50")
-#input_guess("50")
-#input_guess("50")
+# range100()
+# secret_number = 28
+# input_guess("50")
+# input_guess("50")
+# input_guess("50")
+# input_guess("50")
+# input_guess("50")
+# input_guess("50")
+# input_guess("50")
 
 ###################################################
 # Output from test #3
-#New game. Range is [0,100)
-#Number of remaining guesses is 7
+# New game. Range is [0,100)
+# Number of remaining guesses is 7
 #
-#Guess was 50
-#Number of remaining guesses is 6
-#Lower!
+# Guess was 50
+# Number of remaining guesses is 6
+# Lower!
 #
-#Guess was 50
-#Number of remaining guesses is 5
-#Lower!
+# Guess was 50
+# Number of remaining guesses is 5
+# Lower!
 #
-#Guess was 50
-#Number of remaining guesses is 4
-#Lower!
+# Guess was 50
+# Number of remaining guesses is 4
+# Lower!
 #
-#Guess was 50
-#Number of remaining guesses is 3
-#Lower!
+# Guess was 50
+# Number of remaining guesses is 3
+# Lower!
 #
-#Guess was 50
-#Number of remaining guesses is 2
-#Lower!
+# Guess was 50
+# Number of remaining guesses is 2
+# Lower!
 #
-#Guess was 50
-#Number of remaining guesses is 1
-#Lower!
+# Guess was 50
+# Number of remaining guesses is 1
+# Lower!
 #
-#Guess was 50
-#Number of remaining guesses is 0
-#You ran out of guesses.  The number was 28
+# Guess was 50
+# Number of remaining guesses is 0
+# You ran out of guesses.  The number was 28
 #
-#New game. Range is [0,100)
-#Number of remaining guesses is 7
+# New game. Range is [0,100)
+# Number of remaining guesses is 7
